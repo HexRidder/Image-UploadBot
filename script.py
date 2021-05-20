a697f83b10a6d52d9cf495acec183e86f9e3ca8e
廾㠪〤Rɪᴅᴇʀ <84140090+HexRidder@users.noreply.github.com>
2021-05-20 16:54:21 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,3 +1,20 @@
 class script(object):
 
-Help_data
+help_data = """Hey.. It's not that complicated
+
+Follow These steps..
+
+🌀 Send any Image, Gif or Video(Mp4 oNly) below 5MB
+
+🌀 Wait for the link to get generated
+
+NOTE : Currently I don't support text to telegraph"""
+About_data = <b>⭕️ My Name : TroJanZ Telegraph BOT
+        
+⭕️ Creater : @NexonHEX
+
+⭕️ Language : Python3
+
+⭕️ Library : Pyrogram asyncio 0.16.1
+
+⭕️ Source Code : 🔐</b>
