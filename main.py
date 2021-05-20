86b766a4a74dd9a18b03a3400b57e944fb691355
廾㠪〤Rɪᴅᴇʀ <84140090+HexRidder@users.noreply.github.com>
2021-05-20 20:28:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,15 +21,7 @@ TGraph = Client(
 @TGraph.on_message(filters.command("start"))
 async def start(client, message):
     await message.reply_text(
-        text=f"<b>Hai,
-
-I'm a simple Telegraph Uploader bot💯
-
-I can convert gif, image or video(Mp4only) into telegra.ph links
-
-Click help for more details...
-
-You must subscribe our channel in order to use me😇</b>",
+        text=script.start_data,
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup(
                 [
@@ -50,6 +42,57 @@ You must subscribe our channel in order to use me😇</b>",
     except Exception:
         pass
     )
+
+@TGraph.on_message(filters.command(["help"]) & filters.private)
+async def help(client, message):
+    try:
+        await message.reply_text(
+            text=script.HELP_MSG,
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("Back", callback_data="start_data"),
+                        InlineKeyboardButton("About", callback_data="about_data"),
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            "Support",
+                            url="https://telegram.me/Nexonsupport",
+                        )
+                    ],
+                ]
+            ),
+            reply_to_message_id=message.message_id,
+        )
+    except Exception:
+        pass
+
+
+@Tgraph.on_message(filters.command(["about"]) & filters.private)
+async def about(client, message):
+    try:
+        await message.reply_text(
+            text=script.ABOUT_MSG,
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("BACK", callback_data="help_data"),
+                        InlineKeyboardButton("START", callback_data="start_data"),
+                    ],
+                    [
+                        InlineKeyboardButton(
+                            "Support",
+                            url="https://telegram.me/NexonSupport",
+                        )
+                    ],
+                ]
+            ),
+            reply_to_message_id=message.message_id,
+        )
+    except Exception:
+        pass
 
 
 @TGraph.on_message(filters.photo)
